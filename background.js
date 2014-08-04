6ff09156172f71f44b9f86224db3069c733d844c
Richard Laffers <richard.laffers@movys.sk>
2014-08-04 16:28:01 +0200

    fix whitespace

--- background.js
+++ background.js
@@ -1,32 +1,32 @@
 chrome.extension.onRequest.addListener(function(request) {
-	if (request.command !== 'toConsole') {
-			return;
-	}
-	var msg, level, method;
-	msg = request.msg;
-	level = request.level;
-	switch (level) {
-			case 'error':
-					method = 'error';
-					break;
+    if (request.command !== 'toConsole') {
+        return;
+    }
+    var msg, level, method;
+    msg = request.msg;
+    level = request.level;
+    switch (level) {
+        case 'error':
+            method = 'error';
+            break;
 
-			case 'warning':
-					method = 'warn';
-					break;
+        case 'warning':
+            method = 'warn';
+            break;
 
-			case 'info':
-					method = 'info';
-					break;
+        case 'info':
+            method = 'info';
+            break;
 
-			case 'debug':
-			case 'log':
-			default:
-					method = 'log';
-					break;
-	}
-	chrome.tabs.executeScript(request.tabId, {		
-			code: "console." + method + "(" + msg + ");",
-	});
+        case 'debug':
+        case 'log':
+        default:
+            method = 'log';
+            break;
+    }
+    chrome.tabs.executeScript(request.tabId, {      
+        code: "console." + method + "(" + msg + ");",
+    });
 });
 
 /*
@@ -34,41 +34,41 @@ var buffers = {};
 var profiles = {};
 var pattern = /^firelogger-([0-9a-f]+)-(\d+)/i;
 var parseHeader = function(name, value) {
-	var res = pattern.exec(name);
-	if (!res) return;
-	buffers[res[1]] = buffers[res[1]] || [];
-	buffers[res[1]][res[2]] = value;
+    var res = pattern.exec(name);
+    if (!res) return;
+    buffers[res[1]] = buffers[res[1]] || [];
+    buffers[res[1]][res[2]] = value;
 }
 for (var key in localStorage) {
-	parseHeader(key, localStorage[key]);
+    parseHeader(key, localStorage[key]);
 }
 var packets = [];
 for (var bufferId in buffers) {
-	if (!buffers.hasOwnProperty(bufferId)) continue;
-	var buffer = buffers[bufferId].join('');
-	buffer = Base64.decode(buffer);
-	buffer = Utf8.decode(buffer);
-	var packet = JSON.parse(buffer);
-	packets.push(packet);
+    if (!buffers.hasOwnProperty(bufferId)) continue;
+    var buffer = buffers[bufferId].join('');
+    buffer = Base64.decode(buffer);
+    buffer = Utf8.decode(buffer);
+    var packet = JSON.parse(buffer);
+    packets.push(packet);
 }
 var logs = [];
 for (var packet in packets) {
-	var packet = packets[packet];
-	for (i=0; i < packet.logs.length; i++) {
-		var log = packet.logs[i];
-		logs.push(log);
-	}
+    var packet = packets[packet];
+    for (i=0; i < packet.logs.length; i++) {
+        var log = packet.logs[i];
+        logs.push(log);
+    }
 }
 var final = [];
 for (var log in logs) {
-	//final.push(JSON.stringify(logs[log].args));
-	console.info(JSON.stringify(logs[log].args));
+    //final.push(JSON.stringify(logs[log].args));
+    console.info(JSON.stringify(logs[log].args));
 }
 
-X-FireLogger	1.1
-X-FireLoggerProfiler	1
-X-FireLoggerAppstats	1
-x-insight	activate
+X-FireLogger    1.1
+X-FireLoggerProfiler    1
+X-FireLoggerAppstats    1
+x-insight   activate
 */
 
 /*
